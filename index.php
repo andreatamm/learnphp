4430b10b30e1911eade1dace14cd4e394aaff71a
Andrea Tamm <tamm.andrea2005ggmail.com>
2025-09-04 11:46:24 +0300

    Add if and switch examples

--- index.php
+++ index.php
@@ -1,57 +1,49 @@
 <?php
+$num = 3;
+if($num > 5) {
+    var_dump('Bigger then 5');
+} elseif ($num >= 5) {
+    var_dump('Equal to 5');
+} else {
+    var_dump('Smaller then 5');
+}
+$day = (int) date('N', strtotime('05.09.2025'));
+var_dump($day);
 
-$bool = true && false;
+if($day == 1) {
+    var_dump('Esmaspäev');
+} else if ($day == 2) {
+     var_dump('Teisipäev');
+} else if ($day == 3) {
+     var_dump('Kolmapäev');
+} else if ($day == 4) {
+     var_dump('Neljapäev');
+} else if ($day == 5) {
+     var_dump('Pidupäev');
+} else if ($day == 6) {
+     var_dump('Pidupäev');
+} else if ($day == 7) {
+     var_dump('Pühapäev');
+} 
 
-var_dump($bool);
-
-$bool = true || false;
-
-var_dump($bool);
-
-$bool = !true;
-
-var_dump($bool);
-
-$bool = !false;
-
-var_dump($bool);
-
-$bool = 10 > 5;
-
-var_dump($bool);
-
-$bool = 10 < 5;
-
-var_dump($bool);
-
-$bool = 10 < 10;
-
-var_dump($bool);
-
-$bool = 10 == 10;
-
-var_dump($bool);
-
-$bool = 10 >= 10;
-
-var_dump($bool);
-
-$bool = 10 <= 10;
-
-var_dump($bool);
-
-$bool = 10 == '10'; //true
-
-var_dump($bool);
-
-$bool = 10 === '10'; //false
-
-var_dump($bool);
-
-
-$bool = 10 != 10;
-
-$bool = 10 !== 10;
-        // true
-$bool = (true && false || true && !false) && true;
-var_dump($bool);
+switch($day) {
+    case 1:
+        var_dump('Esmaspäev');
+        break;
+    case 2:
+        var_dump('Teisipäev');
+        break;
+    case 3:
+        var_dump('Kolmapäev');
+        break;
+    case 4:
+        var_dump('Neljapäev');
+        break;
+    case 5:
+    case 6:
+        var_dump('Pidupäev');
+        break;
+    case 7:
+        var_dump('Pühapäev');
+        break;
+}
